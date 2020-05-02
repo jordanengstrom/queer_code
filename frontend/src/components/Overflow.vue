ed33599b85a55af1ba452ba88f8f1a335534b5c3
Jordan Engstrom <jordan.engstrom@gmail.com>
2020-05-01 21:29:52 -0600

    fixed background cut off issue

--- frontend/src/components/Overflow.vue
+++ frontend/src/components/Overflow.vue
@@ -43,7 +43,7 @@ export default {
 <style scoped>
 .comms {
   background-image: linear-gradient(-225deg, #e3fdf5 0%, #ffe6fa 100%);
-  height: 100vh;
+  height: 100%;
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
